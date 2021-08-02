04c1b82c0f1ec72cd6c450875a18717fe3da46ba
Andreia Scariot Beulke <andreia.scariot.b@gmail.com>
2021-08-02 11:46:02 -0300

    Estado e Cidade

---
+++ src/main/java/com/utfpr/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.utfpr.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.utfpr.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
