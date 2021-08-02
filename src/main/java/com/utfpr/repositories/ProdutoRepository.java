5b4132378ad5b0d90fc37becab74c4420c0331c9
Andreia Scariot Beulke <andreia.scariot.b@gmail.com>
2021-08-02 10:39:11 -0300

    Produto e associação muitos para muitos

---
+++ src/main/java/com/utfpr/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.utfpr.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.utfpr.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
